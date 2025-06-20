bff7d8e82a8ea864ad2c226daf963aa5d97d3980
JoyceQiao7 <joyce_qiao@berkeley.edu>
2025-06-20 11:29:27 -0700

    add comment on voice ai usage

--- frontend/src/components/VoiceSearch.tsx
+++ frontend/src/components/VoiceSearch.tsx
@@ -3,3 +3,5 @@ import React from 'react';
 export default function VoiceSearch() {
   return <div>Voice search (Vapi) integration</div>;
 } 
+
+// Voice AI combined with geographic location of user could be used to set real-time alert. 
